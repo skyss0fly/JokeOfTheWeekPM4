d07fedd6d38feb49fe7819cc15879d574cd2e860
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2022-07-07 11:50:02 +1000

    Update Main.php

--- JokeOfTheWeekPM4/src/Main.php
+++ JokeOfTheWeekPM4/src/Main.php
@@ -26,7 +26,7 @@ class JokeOfTheWeekPM4 extends PluginBase implements Listener{
 
 	public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
 		if ($command->getName() === "jotw") {
-			$sender->echo("Joke: Why do actors have to always go to hospital? Because they always have a CAST");
+			$sender->sendMessage("Joke: Why do actors have to always go to hospital? Because they always have a CAST");
 			return true;
 		}
 		return true;
