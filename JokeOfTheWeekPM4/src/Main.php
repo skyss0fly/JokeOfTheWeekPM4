eac122f9733f7f408885f3746dc0ebe17ae969f3
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2022-07-07 11:40:03 +1000

    Update Main.php

--- JokeOfTheWeekPM4/src/Main.php
+++ JokeOfTheWeekPM4/src/Main.php
@@ -9,6 +9,21 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 
+class JokeOfTheWeekPM4 extends PluginBase implements Listener{
+
+	/** This is a public constant that you can access from anywhere by doing ExamplePlugin::VERSION */
+	public const VERSION = "v1.0.0";
+
+	/** @var self $main */
+	private static $main;
+
+	/** Example of the onEnable function */
+	public function onEnable() : void{
+		self::$main = $this;
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->getLogger()->info("Joke of the week is enabled!");
+	}
+
 	private function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
 		if ($command->getName() === "jotw") {
 			$sender->echo("Joke: Why do actors have to always go to hospital? Because they always have a CAST");
