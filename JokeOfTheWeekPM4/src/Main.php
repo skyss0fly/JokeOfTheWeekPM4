3d9195b599fdf982fdd6f631afeeb4a71ed8c516
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2022-07-07 11:54:56 +1000

    Update Main.php

--- JokeOfTheWeekPM4/src/Main.php
+++ JokeOfTheWeekPM4/src/Main.php
@@ -19,7 +19,7 @@ class JokeOfTheWeekPM4 extends PluginBase implements Listener{
 
 	/** Example of the onEnable function */
 	public function onEnable() : void{
-		self::$JokeOfTheWeekPM4 = $this;
+		self::$this = $this;
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		$this->getLogger()->info("Joke of the week is enabled!");
 	}
